0d1ffb5bff702bb614865235bcd5ef80a9678b82
Simas <simasss2007@gmail.com>
2017-10-31 06:44:45 +0200

    singleton klases testavimo pakeitimai.

---
+++ bomberman_1/services/singleton/servicessingleton.cs
@@ -0,0 +1,32 @@
+﻿using Bomberman_1.services;
+using BomberManProject.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BomberManProject.Singleton
+{
+    class ServicesSingleton
+    {
+        private static ServicesSingleton _instance;
+
+        public CommandsService commandService = new CommandsService();
+        public CoordinatesTransformationService coordinatesTransformationService;
+
+
+        private ServicesSingleton() { }
+
+        public static ServicesSingleton getInstance()
+        {
+            Console.WriteLine("singleton veikia.");
+            if (_instance == null)
+            {
+                return new ServicesSingleton();
+            }
+            return _instance;
+        }
+    }
+}
+
